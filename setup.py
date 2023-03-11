27095cdf383e5ced04d57db0552678a513fc47b4
Naveenkumar B <bnaveen14569@gmail.com>
2023-03-11 22:57:24 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+name = "MLProjects",
+version = '0.0.1',
+author = 'Naveenkumar',
+author_email = 'bnaveen14569@gmail.com',
+packages = find_packages(),
+install_requires = ['pandas', 'numpy', 'seaborn']
+) 
