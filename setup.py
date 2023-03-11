80a8304129183869047e2693d1330484a13ab03b
Naveenkumar B <bnaveen14569@gmail.com>
2023-03-12 00:55:39 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements(fpath:str)->List[str]:
+    requirements = []
+    with open(fpath) as obj:
+        requirements = obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    return requirements
 
 setup(
 name = "MLProjects",
@@ -6,5 +17,6 @@ version = '0.0.1',
 author = 'Naveenkumar',
 author_email = 'bnaveen14569@gmail.com',
 packages = find_packages(),
-install_requires = ['pandas', 'numpy', 'seaborn']
-) 
+install_requires = get_requirements("requirements.txt")
+)  
+     
